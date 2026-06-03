94c5f923baa345be205c61784a90a47e0ce65bdb
Artur Oyakawa <arturoyakawa5@gmail.com>
2026-06-03 01:18:54 -0300

    commenting

--- js/script.js
+++ js/script.js
@@ -3,13 +3,14 @@ const palco = document.getElementById("palco")
 const ctx = palco.getContext("2d")
 
 // DISCLAMER!!!!!!!
-// O código está 
+// O código está em ingles spq estou acostumado a programar assim. Nao copiei da IA!!!!!!
 
 const insetValue = 20
 let width
 let height
 
 const floorAreaFraction = .15
+// 15% da area total sera reservada para o player
 
 function resizeCanvas()
 {
